88445706634981c2e62f9c5308d29bcea9b3678d
rifkyghifari <rifky.ghifari05@gmail.com>
2024-12-24 23:33:08 +0700

    Ubah tampilan harga menjadi rupiah

--- src/utils/CartItem.js
+++ src/utils/CartItem.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { GrSubtractCircle, GrAddCircle } from "react-icons/gr";
 
+const formatPrice = (price) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(price);
+};
+
 const CartItem = ({ item, onIncrement, onDecrement }) => {
   return (
     <div className="flex items-center mb-6 p-4 border rounded-lg shadow-sm">
@@ -12,11 +19,11 @@ const CartItem = ({ item, onIncrement, onDecrement }) => {
       <div className="flex-1">
         <div className="flex justify-between mb-2">
           <h4 className="text-lg font-medium">{item.product_name}</h4>
-          <p className="font-semibold">${item.price}</p>
+          <p className="font-semibold">{formatPrice(item.price)}</p>
         </div>
         <div className="flex justify-between mb-2">
           <p className="text-sm text-gray-600">{item.category_name}</p>
-          <p className="text-sm text-gray-600">{item.discount === null?"": "$"+ item.discount}</p>
+          <p className="text-sm text-gray-600">{item.discount === null?"": "Diskon: " + item.discount + " %"}</p>
         </div>
         <div className="flex justify-between text-sm">
           <p>Note: {item.note}</p>
